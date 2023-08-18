83ab6e459d779812e3406fa9ca8e38d7b32affeb
asalshamim <shamimasal8@gmail.com>
2023-08-18 14:28:07 +0200

    add Movies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import MovieBox from './MovieBox';
 
+
+const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=04585c5f3f14630b5578d643f8d7d76f";
+
+
+
 function App() {
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then(data => {
+        console.log(data)
+        setMovies(data.results);
+      })
+  }, [])
+  
   return (
-    <div className="App">
-    {movies.map((moviesReq)=><MovieBox/>)}
+    <div >
+      {movies.map((moviesReq) =>
+        <MovieBox key={moviesReq.id} {...moviesReq} />)}
     </div>
   );
 }
--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,9 +1,12 @@
 import React from 'react';
+const API_IMG = "https://image.tmdb.org/t/p/w500/";
 
-const MovieBox = () => {
+const MovieBox = ({titel, poster_path, vote_average, release_sate, overview}) => {
   return (
     <div>
-      Movie box
+      <h1>{titel}</h1>
+      <img src={API_IMG + poster_path} alt='poster'></img>
+      <p>{overview}</p>
     </div>
   )
 }
